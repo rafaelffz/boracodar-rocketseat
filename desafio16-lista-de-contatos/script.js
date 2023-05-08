1260eb8c6f5086c4544612c138fe8adda2452162
Rafael <108911706+rafaelffz@users.noreply.github.com>
2023-05-08 17:53:17 -0300

    search issues fix

--- desafio16-lista-de-contatos/script.js
+++ desafio16-lista-de-contatos/script.js
@@ -1,23 +1,31 @@
 const filterElement = document.querySelector('header input')
-const contacts = document.querySelectorAll('.person')
+const contacts = document.querySelectorAll('.list-wrapper')
 
 filterElement.addEventListener('input', filterContact)
 
 function filterContact() {
     if(filterElement.value != '') {
         for(let contact of contacts) {
-            let name = contact.querySelector('h3')
-            name = name.textContent.toLowerCase()
+            const names = contact.querySelectorAll('h3')
+            const filterText = filterElement.value.toLowerCase()
+            const people = document.querySelectorAll('.person')
+            
 
-            let filterText = filterElement.value.toLowerCase()
-            if(!name.includes(filterText)) {
-                // contact.style.display = 'none'
-                contact.classList.add('none')
-            } else {
-                // contact.style.display = 'flex'
-                contact.classList.remove('none')
-                contact.classList.add('flex')
-            }
+            let bool = false
+            names.forEach(name => {
+                if(!name.textContent.toLowerCase().startsWith(filterText) && !bool) {
+                    // contact.style.display = 'none'
+                    contact.classList.add('none')
+                    for(let person of people) {
+                        person.classList.add('none')
+                    }
+                } else {
+                    // contact.style.display = 'flex'
+                    contact.classList.remove('none')
+                    contact.classList.add('flex')
+                    bool = true
+                }
+            }) 
         }
     } else {
         for(let contact of contacts) {
